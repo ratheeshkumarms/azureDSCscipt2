3b8312807b3639161d7df2debb8f2f6a9382ba19
ratheeshkumarms <31408049+ratheeshkumarms@users.noreply.github.com>
2017-08-28 19:03:40 +0530

    Add files via upload

---
+++ Filecreate.ps1
@@ -0,0 +1,11 @@
+﻿Configuration createFile
+{
+    Node "MachineName"
+    {
+        File CreateFile {
+        Destination = 'c:\test.txt'
+        Ensure = "Present"
+        Contents = 'This is a test DSC'
+        }
+    {
+}
---
+++ RegistryEdit.ps1
@@ -0,0 +1,11 @@
+﻿Configuration RegistryEdit
+{
+param ($MachineName)
+    Registry $MachineName
+    {
+        Ensure      = "Present"
+        Key         = "HKEY_LOCAL_MACHINE\SOFTWARE\ExampleKey"
+        ValueName   = "TestValue"
+        ValueData   = "TestData"
+    }
+}
